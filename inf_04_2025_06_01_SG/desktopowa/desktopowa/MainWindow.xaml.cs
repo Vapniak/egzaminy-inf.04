5906945418de14141561a2c0e07307a290ac31e2
Vapniak <113126917+Vapniak@users.noreply.github.com>
2026-01-14 11:28:37 +0100

    Konsolowa i desktopowa 2025 06 01 kolory rgb

---
+++ inf_04_2025_06_01_SG/desktopowa/desktopowa/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace desktopowa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SliderR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Update();
+        }
+
+        private void SliderG_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Update();
+        }
+
+        private void SliderB_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Update();
+        }
+
+        private void ReadValueButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsInitialized)
+            {
+                return;
+            }
+
+            var color = GetColor();
+            RGBValueText.Text = $"{color.R}, {color.G}, {color.B}";
+            RGBValueText.Background = new SolidColorBrush(color);
+        }
+
+        private void Update()
+        {
+            if(!IsInitialized)
+            { 
+                return; 
+            }
+
+            var color = GetColor();
+            
+            ColorBorder.Background = new SolidColorBrush(color);
+
+            LabelR.Content = color.R;
+            LabelG.Content = color.G;
+            LabelB.Content = color.B;
+        }
+
+
+        private Color GetColor()
+        {
+            var r = (byte)SliderR.Value;
+            var g = (byte)SliderG.Value;
+            var b = (byte)SliderB.Value;
+
+
+            return Color.FromRgb(r, g, b);
+        }
+    }
+}
---
+++ inf_04_2025_06_01_SG/konsolowa/konsolowa/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace konsolowa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ile wygenerować losowań?");
+            var n = int.Parse(Console.ReadLine());
+
+            var losowania = new int[n][];
+
+            for (var i = 0; i < n; i++)
+            {
+                losowania[i] = new int[6];
+                WypelnijLosowaniami(losowania[i]);
+            }
+
+            WyswietlLosowania(losowania);
+            WyswietlWystapienia(losowania);
+        }
+
+
+        /**********************************************
+        nazwa funkcji: WypelnijLosowaniami
+        opis funkcji: Wypełnia tablicę pseudolosowymi liczbami w zakresie 1-49 bez powtórzeń.
+        parametry: tab - tablica którą wypełni losowaniami, musi być wcześniej zainicjowana zerami aby została wypełniona
+        zwracany typ i opis: brak
+        autor: <numer zdającego>
+        ***********************************************/
+        public static void WypelnijLosowaniami(int[] tab)
+        {
+            var rand = new Random();
+
+            for(var i = 0; i < tab.Length; i++)
+            {
+                var liczba = 0;
+                do
+                {
+                    liczba = rand.Next(1, 50);
+                } while (tab.Contains(liczba));
+                tab[i] = liczba;
+            }
+        }
+
+
+
+        /**********************************************
+        nazwa funkcji: WyswietlLosowania
+        opis funkcji: Wyświetla wszystkie losowania
+        parametry: tab - tablica z tablicami wszystkich elementów losowań
+        zwracany typ i opis: brak
+        autor: <numer zdającego>
+        ***********************************************/
+        public static void WyswietlLosowania(int[][] tab)
+        {
+
+            Console.WriteLine("Zestawy wylosowanych liczb:");
+            for (var i = 0; i < tab.Length; i++)
+            {
+                Console.Write($"Losowanie {i + 1}: ");
+                foreach (var liczba in tab[i])
+                {
+                    Console.Write($"{liczba} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+
+        /**********************************************
+        nazwa funkcji: WyswietlWystapienia
+        opis funkcji: Zlicza wystąpienia każdej z liczb od 1 do 49 i je wyświetla
+        parametry: tab - tablica z tablicami wszystkich elementów 
+        zwracany typ i opis: brak
+        autor: <numer zdającego>
+        ***********************************************/
+        public static void WyswietlWystapienia(int[][] tab)
+        {
+            var wystapienia = new int[50];
+            for(var i = 0; i < tab.Length; i++)
+            {
+                for(var j = 0; j < tab[i].Length; j++)
+                {
+                    wystapienia[tab[i][j]]++;
+                }
+            }
+
+            for(var i = 1; i < wystapienia.Length; i++)
+            {
+                Console.WriteLine($"Wystąpienia liczby {i}: {wystapienia[i]}");
+            }
+        }
+    }
+}
